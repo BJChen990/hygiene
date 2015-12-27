f239ba91d998dedb5b3c998a306faa859224e5d7
chenbangjing <bengjing@gmail.com>
2015-12-27 22:05:01 +0800

    fixing

--- clean.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'chenbangjing'
-import os
-
-if __name__ == '__main__':
-    for file in os.listdir('./'):
-        if file.endswith('.xls') or file.endswith('.sqlite3'):
-            print('remove'+file)
-            os.remove(file)
