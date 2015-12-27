d8605ebc1d3b0067b0018549da9d43f856da6a9a
chenbangjing <bengjing@gmail.com>
2015-12-27 21:44:27 +0800

    fixing

--- index/models.py
+++ index/models.py
@@ -6,6 +6,7 @@ class User(models.Model):
     user_id = models.CharField(max_length=20)
     type = models.CharField(max_length=20, default='manager')
 
+
 class Class(models.Model):
     grade = models.TextField()
     number = models.PositiveSmallIntegerField(default=0)
